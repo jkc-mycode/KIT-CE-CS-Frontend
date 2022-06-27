bd6d8a241490691f5b5c16cffd5ba9e523635230
khj745700 <khj745700@naver.com>
2022-06-27 14:55:08 +0900

    Init front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+    let [post, setPost] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className = "header">
+            <div className = "items">CE-SE</div>
+            <div className = "items">login</div>
+            <div className = "items">MyPage</div>
+        </div>
+        <div className = "menu_fixed">
+            <div className = "menubar">
+                <div className = "menu_items">전체 게시판</div>
+                <div className = "menu_items">졸업생 게시판</div>
+                <div className = "menu_items">질문 게시판</div>
+                <div className = "menu_items">학업 게시판</div>
+                <div className = "menu_items">차후 추가 예정</div>
+                <div className = "menu_items">차후 추가 예정</div>
+            </div>
+        </div>
+        <div className = "board">
+            <table>
+                <tr>
+                    <td>번호</td>
+                    <td>제목</td>
+                    <td>작성자</td>
+                    <td>작성일</td>
+                    <td>조회수</td>
+                </tr>
+                <td>더미데이터</td>
+            </table>
+        </div>
     </div>
   );
 }
