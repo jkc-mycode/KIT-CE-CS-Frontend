245bace529aa797a5280236f00079945bb89e66e
jkc-mycode <jkc9709@naver.com>
2022-06-28 13:16:57 +0900

    22/06/28 로그인 페이지 코드 수정

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,19 +1,28 @@
 import React, {useState} from 'react';
 
 function LoginPage(){
-    const [ID, setID] = useState("");
-    const [PW, setPW] = useState("");
+    const [id, setid] = useState("");
+    const [password, setpassword] = useState("");
+
+    const onIdHandler = (event) => {
+        setid(event.currentTarget.value);
+    }
+    const onPasswordHandler = (event) => {
+        setpassword(event.currentTarget.value);
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
 
     return (
         <div className="login_box">
             <h2 className="login_title">Login</h2>
             <form>
-                <input type="text" name="ID" value={ID} placeholder="아이디" className="id_input"/><br/>
-                <input type="password" name="PW" value={PW} placeholder="비밀번호" className="pw_input"/><br/>
+                <input type="text" name="id" value={id} placeholder="아이디" className="id_input" onChange={onIdHandler} /><br/>
+                <input type="password" name="password" value={password} placeholder="비밀번호" className="pw_input" onChange={onPasswordHandler} /><br/>
                 <button type="submit" className="login_button">로그인</button>
             </form>
         </div>
     )
 }
-
 export default LoginPage;
