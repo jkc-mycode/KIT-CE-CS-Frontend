5bc93dc19ee3aa73b2eb1e246c4fb489c42158fd
jkc-mycode <jkc9709@naver.com>
2022-06-30 12:42:20 +0900

    22/06/30 게시글 상세보기 페이지 수정

--- src/Components/View.js
+++ src/Components/View.js
@@ -2,46 +2,62 @@ import React, {useState} from 'react';
 import './view_test.css';
 
 function ViewPage(){
-    const [id, setId] = useState("");
-    const [password, setPassword] = useState("");
+    const [post, setPost] = useState({});
 
-    const onIdHandler = (event) => {
-        setId(event.currentTarget.value);
-    }
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    }
-    const onSubmit = (event) => {
-        event.preventDefault();
-    }
+    //useEffect() 처리
 
     return (
         <>
-            <div className="test">페이지 이름, 로그인, 마이페이지 메뉴 위치</div>
-            <div className="test">네이게이션 메뉴 바 위치</div>
-            <h1>게시글 타이틀</h1>
+            <div className = "header">
+                <div className = "items">CE-SE</div>
+                <div className = "items">login</div>
+                <div className = "items">MyPage</div>
+            </div>
+            <div className = "menu_fixed">
+                <div className = "menubar">
+                    <div className = "menu_items">전체 게시판</div>
+                    <div className = "menu_items">졸업생 게시판</div>
+                    <div className = "menu_items">질문 게시판</div>
+                    <div className = "menu_items">학업 게시판</div>
+                    <div className = "menu_items">차후 추가 예정</div>
+                    <div className = "menu_items">차후 추가 예정</div>
+                </div>
+            </div>
+            <h1 className="title">게시글 타이틀</h1> {/* {post.title} */}
             <div>
-                <table>
+                <table className="post_info_table">
                     <tr>
-                        <td>작성자</td>
-                        <td>작성일 &nbsp;</td>
-                        <td>조회수 &nbsp;</td>
+                        <td>작성자</td> {/* {post.author} */}
+                        <td>작성일 &nbsp;</td> {/* {post.date} */}
+                        <td>조회수 &nbsp;</td> {/* {post.hit} */}
                     </tr>
                 </table>
-                <div className="test" style={{height: '400px'}}>더미 데이터 게시글</div>
+                <div className="test" style={{height: '400px'}}>더미 데이터 게시글</div> {/* {post.content} */}
+                <div className="test" >태그 정보</div> {/* {post.tag} */}
             </div>
             <br/>
             <div>
-                <table>
+                <table className="revise_delete_list">
                     <tr>
-                        <td>수정</td>
-                        <td>삭제</td>
-                        <td>목록</td>
+                        <td>수정(버튼)</td>
+                        <td>삭제(버튼)</td>
+                        <td>목록(버튼)</td>
                     </tr>
                 </table>
             </div>
-            <div className="test">댓글 메뉴 위치</div>
-            <div className="test">게시글 목록</div>
+            <div className="test">댓글 메뉴 위치 (예정)</div><br/>
+            <div className="board">
+                <table>
+                    <tr>
+                        <td>번호</td>
+                        <td>제목</td>
+                        <td>작성자</td>
+                        <td>작성일</td>
+                        <td>조회수</td>
+                    </tr>
+                    <td>더미데이터</td>
+                </table>
+            </div>
         </>
     )
 }
