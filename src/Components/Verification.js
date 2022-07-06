2afd2e220b8a93d66e991f4b62cbf77dde8fdb1b
seongjxn <seongjxn@ivyforest.kr>
2022-07-06 16:10:47 +0900

    22.07.06 Verification.css 생성 및 Verification.js 수정

--- src/Components/Verification.js
+++ src/Components/Verification.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './Verification.css';
 
 function VerificationPage(){
     const [verification, setVerification] = useState("");
@@ -12,10 +13,13 @@ function VerificationPage(){
 
     return (
         <div className="verification_box">
-            <h2 className="verification_title">Verification</h2>
+            <h1 className="verification_title">&#xE001;_ Verification</h1>
+            <h3 className="message">입력하신 웹메일로 인증 코드가 전송되었습니다.<br/>메일함을 확인 후 인증 코드를 입력해 주세요.</h3>
             <form>
                 <input type="text" name="verification" value={verification} placeholder="인증코드" className="verification_input" onChange={onVerificationHandler} /><br/>
-                <button type="submit" className="verification_button" onSubmit={onSubmit} >인증하기</button>
+                <div className="button_container">
+                    <button type="submit" className="verification_button" onSubmit={onSubmit} >인증하기</button>
+                </div>
             </form>
         </div>
     )
