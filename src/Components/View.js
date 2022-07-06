94a2d4a911df0ace19f3d7d71c63ea42ba4569cc
seongjxn <seongjxn@ivyforest.kr>
2022-07-06 15:31:53 +0900

    22.07.06 View.css View.js 수정

--- src/Components/View.js
+++ src/Components/View.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './view.css';
+import './View.css';
 
 
 import Header from './header';
@@ -13,29 +13,33 @@ function ViewPage(){
 
     return (
         <>
-            <h1 className="title">게시글 타이틀</h1> {/* {post.title} */}
-            <div>
-                <table className="post_info_table">
-                    <tr>
-                        <td>작성자</td> {/* {post.author} */}
-                        <td>작성일 &nbsp;</td> {/* {post.date} */}
-                        <td>조회수 &nbsp;</td> {/* {post.hit} */}
-                    </tr>
-                </table>
-                <div className="test" style={{height: '400px'}}>더미 데이터 게시글</div> {/* {post.content} */}
-                <div className="test" >태그 정보</div> {/* {post.tag} */}
+            <div className="post_section">
+                <h1 className="title">&#xE001;_ 게시글 타이틀</h1> {/* {post.title} */}
+                <div>
+                    <table className="post_info_table">
+                        <tr>
+                            <td>작성자</td> {/* {post.author} */}
+                            <td>조회수 &nbsp;</td> {/* {post.hit} */}
+                            <td>작성일 &nbsp;</td> {/* {post.date} */}
+                        </tr>
+                    </table>
+                    <div className="line"></div>
+                    <div className="post_content" style={{height: '400px'}}>더미 데이터 게시글</div> {/* {post.content} */}
+                    <div className="post_tag" >태그 정보</div> {/* {post.tag} */}
+                </div>
+                <div>
+                    <table className="edit_delete_list">
+                        <tr>
+                            <td>수정</td>
+                            <td>삭제</td>
+                            <td>목록</td>
+                        </tr>
+                    </table>
+                    <div className="line"></div>
+                </div>
+                <div className="test">댓글 메뉴 위치 (예정)</div>
             </div>
             <br/>
-            <div>
-                <table className="revise_delete_list">
-                    <tr>
-                        <td>수정(버튼)</td>
-                        <td>삭제(버튼)</td>
-                        <td>목록(버튼)</td>
-                    </tr>
-                </table>
-            </div>
-            <div className="test">댓글 메뉴 위치 (예정)</div><br/>
             <BoardList/>
         </>
     )
