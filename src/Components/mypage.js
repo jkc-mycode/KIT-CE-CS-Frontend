3df913b16ee5ad8b5070fb35d65af56b405d3a76
seongjxn <seongjxn@ivyforest.kr>
2022-07-06 19:49:00 +0900

    22.07.06 mypage.js 수정

--- src/Components/mypage.js
+++ src/Components/mypage.js
@@ -2,9 +2,6 @@ import React, {useState} from 'react';
 import './mypage.css';
 
 function RegisterPage(){
-    const [name, setName] = useState(""); //이름
-    const [webmail, setWebmail] = useState(""); //웹메일
-    const [id, setId] = useState(""); //아이디
     const [password, setPassword] = useState(""); //비밀번호
     const [confirmPassword, setConfirmPassword] = useState(""); //비밀번호 확인
 
@@ -15,15 +12,15 @@ function RegisterPage(){
             <form>
                 <div className="mypage_msg">이름</div>
                 <div className="mypage_row">
-                    <input type="text" name="name" value={name} placeholder="Name" className="name_show" onChange={onNameHandler} /><br/>
+                    <div className="name_show">Name</div> {/* {user.name} */}
                 </div>
                 <div className="mypage_msg">금오공대 웹메일</div>
                 <div className="mypage_row">
-                    <input type="email" name="webmail" value={webmail} placeholder="WebMail@kumoh.ac.kr" className="webmail_show" onChange={onMailHandler} /><br/>
+                    <div className="webmail_show">WebMail@kumoh</div> {/* {user.name} */}
                 </div>
                 <div className="mypage_msg">아이디</div>
                 <div className="mypage_row">
-                    <input type="text" name="id" value={id} placeholder="ID" className="_id_show" onChange={onIdHandler} /><br/>
+                    <div className="_id_show">ID</div> {/* {user.id} */}
                 </div>
                 <div className="mypage_msg">비밀번호</div>
                 <div className="mypage_row">
@@ -33,10 +30,10 @@ function RegisterPage(){
                     <input type="password" name="confirmPassword" value={confirmPassword} placeholder="Password Confirm" className="confirm_pw_input" onChange={onConfirmPasswordHandler} /><br/>
                 </div>
                 <div className="button_container">
-                    <button type="submit" className="pw_edit_button" onSubmit={onSubmit} >비밀번호 변경</button>
+                    <button className="pw_edit_button" onSubmit={onSubmit} >비밀번호 변경</button>
                 </div>
             </form>
         </div>
     )
 }
-export default RegisterPage;
+export default MyPage;
