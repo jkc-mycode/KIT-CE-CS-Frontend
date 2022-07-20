3bb22a6b4f06a5d3820d2ebf2ef5d3552453c1dc
seongjxn <seongjxn@ivyforest.kr>
2022-07-20 19:35:22 +0900

    22.07.20 menubar line 추가

--- src/Components/menubar.js
+++ src/Components/menubar.js
@@ -3,14 +3,17 @@ import './menubar.css';
 
 function MenuBar(){
     return (
-
-            <div className = "menubar">
-                <div className = "menubar_items">전체 게시판</div>
-                <div className = "menubar_items">공지사항</div>
-                <div className = "menubar_items">학업 게시판</div>
-                <div className = "menubar_items">자유 게시판</div>
-                <div className = "menubar_items">졸업생 게시판</div>
-            </div>
+        <div className = "menubar">
+            <div className = "menubar_items">전체 게시판</div>
+            <div className="menubar_line"></div>
+            <div className = "menubar_items">공지사항</div>
+            <div className="menubar_line"></div>
+            <div className = "menubar_items">학업 게시판</div>
+            <div className="menubar_line"></div>
+            <div className = "menubar_items">자유 게시판</div>
+            <div className="menubar_line"></div>
+            <div className = "menubar_items">졸업생 게시판</div>
+        </div>
     )
 }
 
