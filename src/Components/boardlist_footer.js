a7a5bf6978f25cc6601ab53a12d01c6f4af45eaa
jkc-mycode <jkc9709@naver.com>
2022-07-25 22:25:24 +0900

    게시글 작성 페이지, 게시판타입 드롭다운, 게시물 만들기 버튼 코드 작성

---
+++ src/Components/boardlist_footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './menubar.css';
+import {useNavigate} from 'react-router-dom';
+
+function BoardFooter(){
+    const navigate = useNavigate();
+    const loginCheck = () => {
+        if(window.sessionStorage.getItem("id") === null){
+            alert("로그인 후 이용 가능합니다.");
+        }else{
+            navigate('/post_write');
+        }
+    }
+    return (
+        <div className = "boardlist_footer">
+            <button type="button" className="post_write" onClick={loginCheck}>글쓰기</button>
+        </div>
+    )
+}
+
+export default BoardFooter;
