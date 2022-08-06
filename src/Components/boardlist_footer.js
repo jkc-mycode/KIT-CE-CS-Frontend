edabc7d323cd91296449bc7ed6719b54fdd0b11e
jkc-mycode <jkc9709@naver.com>
2022-08-06 16:31:21 +0900

    boardlist_footer 필요없는 내용 삭제

--- src/Components/boardlist_footer.js
+++ src/Components/boardlist_footer.js
@@ -1,10 +1,8 @@
 import React from "react";
 import './menubar.css';
 import {useNavigate} from 'react-router-dom';
-import styled from "styled-components";
 
-function BoardFooter({ total, limit, page, setPage }){
-    const numPages = Math.ceil(total / limit);
+function BoardFooter() {
     const navigate = useNavigate();
     const loginCheck = () => {
         if(window.sessionStorage.getItem("id") === null){
@@ -19,42 +17,5 @@ function BoardFooter({ total, limit, page, setPage }){
         </div>
     )
 }
-
-const Nav = styled.nav`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 4px;
-  margin: 16px;
-`;
-
-const Button = styled.button`
-  border: none;
-  border-radius: 8px;
-  padding: 8px;
-  margin: 0;
-  background: black;
-  color: white;
-  font-size: 1rem;
-
-  &:hover {
-    background: tomato;
-    cursor: pointer;
-    transform: translateY(-2px);
-  }
-
-  &[disabled] {
-    background: grey;
-    cursor: revert;
-    transform: revert;
-  }
-
-  &[aria-current] {
-    background: deeppink;
-    font-weight: bold;
-    cursor: revert;
-    transform: revert;
-  }
-`;
 
 export default BoardFooter;
