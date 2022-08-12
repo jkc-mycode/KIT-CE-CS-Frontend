4ebb274388c65a8bf498b9ba6c261ff2165d61c7
hj-language <idgafff@kumoh.ac.kr>
2022-08-12 20:14:31 +0900

    22.08.12 권한 관련 리액트 쿠키 이용하게끔 변경

--- src/Components/boardlist_footer.js
+++ src/Components/boardlist_footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './menubar.css';
 import {useNavigate} from 'react-router-dom';
+import { getCookie } from '../cookie';
 
 function BoardFooter() {
     const navigate = useNavigate();
     const loginCheck = () => {
-        if(window.sessionStorage.getItem("id") === null){
+        if(!getCookie("kit_acs")){
             alert("로그인 후 이용 가능합니다.");
+            navigate('/login')
         }else{
             navigate('/post_write');
         }
