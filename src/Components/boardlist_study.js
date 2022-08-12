0f97cd139636e7bd1ddf17d0b68e028b91bddfc6
hj-language <idgafff@kumoh.ac.kr>
2022-08-13 02:01:28 +0900

    22.08.13 boardlist body_section 통일

--- src/Components/boardlist_study.js
+++ src/Components/boardlist_study.js
@@ -6,26 +6,28 @@ import Crawling from './crawling';
 function BoardList() {
     return(
         <div className = "view_section">
-            <div className="left_section">
-                <div className="nowBoard">&#xE001;_ 학업 게시판</div>
-                <table className = "board_list_table">
-                    <thead className = "board_head">
-                    <tr>
-                        <th>번호</th>
-                        <th>제목</th>
-                        <th>작성자</th>
-                        <th>작성일</th>
-                        <th>조회수</th>
-                    </tr>
-                    </thead>
-                    <tbody className = "board_body">
-                    <BoardRow/>
-                    </tbody>
-                </table>
-            </div>
-            <div className="margin_section"></div>
-            <div className="right_section">
-                <Crawling></Crawling>
+            <div className = "body_section">
+                <div className="left_section">
+                    <div className="nowBoard">&#xE001;_ 학업 게시판</div>
+                    <table className = "board_list_table">
+                        <thead className = "board_head">
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
+                            <th>작성일</th>
+                            <th>조회수</th>
+                        </tr>
+                        </thead>
+                        <tbody className = "board_body">
+                        <BoardRow/>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="margin_section"></div>
+                <div className="right_section">
+                    <Crawling></Crawling>
+                </div>
             </div>
         </div>
     )
