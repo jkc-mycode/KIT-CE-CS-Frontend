bad5861240948dc09102669b8489b46a9b29a25b
ssam2s <ssam2s@naver.com>
2022-08-13 00:24:58 +0900

    08.13 12:24 UI FIX

--- src/Components/boardlist_free.js
+++ src/Components/boardlist_free.js
@@ -1,14 +1,13 @@
 import React from "react";
 import './boardlist.css';
 import BoardRow from './boardrow';
-import Crawling from './crawling';
-
-
+import Crawling from './crawling.js';
+import BoardFooter from './boardlist_footer';
 
 function BoardList() {
     return(
         <div className = "view_section">
-            <div className="body_section">
+            <div className = "body_section">
             <div className="left_section">
                 <div className="nowBoard">&#xE001;_ 자유 게시판</div>
                 <table className = "board_list_table">
@@ -22,9 +21,11 @@ function BoardList() {
                     </tr>
                     </thead>
                     <tbody className = "board_body">
+                    <BoardRow />
                     </tbody>
                 </table>
-                <BoardRow/>
+                    <BoardFooter />
+                
             </div>
             <div className="margin_section"></div>
             <div className="right_section">
--- src/Components/boardlist_notice.js
+++ src/Components/boardlist_notice.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './boardlist.css';
 import BoardRow from './boardrow';
-import Crawling from './crawling';
-
+import Crawling from './crawling.js';
+import BoardFooter from './boardlist_footer';
 
 function BoardList() {
     return(
@@ -14,6 +14,7 @@ function BoardList() {
                     <thead className = "board_head">
                     <tr>
                         <th>번호</th>
+                        <th>카테고리</th>
                         <th>제목</th>
                         <th>작성자</th>
                         <th>작성일</th>
@@ -21,9 +22,11 @@ function BoardList() {
                     </tr>
                     </thead>
                     <tbody className = "board_body">
+                    <BoardRow />
                     </tbody>
                 </table>
-                <BoardRow/>
+                    <BoardFooter />
+                
             </div>
             <div className="margin_section"></div>
             <div className="right_section">
