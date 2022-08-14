106324b9a8179d056e0d965c56b56381617ba948
jkc-mycode <jkc9709@naver.com>
2022-08-14 17:55:33 +0900

    2022.08.14 페이지네이션 boardrow에 넣어서 사용하는 버전(정상동작)

--- src/Components/boardlist_footer.js
+++ src/Components/boardlist_footer.js
@@ -6,11 +6,14 @@ import {useNavigate, useLocation} from 'react-router-dom';
 import axios from "axios";
 
 function BoardFooter() {
-    const [list, setList] = useState([]);
-    const [page, setPage] = useState(1);
+    const location = useLocation();
+    const obj = new URLSearchParams(location.search);
+    const [page, setPage] = useState(Number(obj.get("pageNum")) || 1);
     const [limit, setLimit] = useState(2); //20개 고정
     const [total, setTotal] = useState(12); //전체 게시물 수
-    const location = useLocation();
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
     const navigate = useNavigate();
     const loginCheck = () => {
@@ -22,35 +25,55 @@ function BoardFooter() {
         }
     }
     const handlePageChange = (page) => {
+        let obj = new URLSearchParams(location.search);
         setPage(page);
+        if(obj.get("title")){
+            navigate(location.pathname + "?pageNum=" + page + "&title=" + obj.get("title"));
+        }else if(obj.get("content")){
+            navigate(location.pathname + "?pageNum=" + page + "&content=" + obj.get("content"));
+        }else{
+            navigate(location.pathname + "?pageNum=" + page);
+        }
     };
     const getList = async () => {
-        //const posts = await axios.get("http://kittaxipool.iptime.org:3000/article/")
-        const posts = await axios.get("http://localhost:3001/article" + location.pathname + "/?pageNum=" + page) //뒤에 pagenum붙여서 보내는 걸로
+        let url = "/article"+ location.pathname + "?pageNum=" + page
+        if(!title && !content)
+            url = "/article"+ location.pathname + "?pageNum=" + page
+        else if (title)
+            url = url + "&title="+title
+        else if (content)
+            url += "&content="+content
+        console.log(url);
+        const posts = await axios.get(url)
         console.log(posts);
-        const copy = posts.data.articles.slice(); //slice()는 배열의 복사복을 만듦
-        var _list = copy.reverse();
-        setList(_list);
         setTotal(posts.data.totalArticle);
         setLimit(posts.data.postLimit);
     }
     useEffect(() => {
+        const obj = new URLSearchParams(location.search)
+        if(Number(obj.get("pageNum")) === 0){
+            setPage(1);
+        }else{
+            setPage(Number(obj.get("pageNum")));
+        }
+        setTitle(obj.get("title"));
+        setContent(obj.get("content"));
         getList();
-    }, [page])
+    }, [page, location])
 
     return (
         <div className = "boardlist_footer">
             <button type="button" className="post_write" onClick={loginCheck}>글쓰기</button>
-            <Pagination
-                activePage={page} //현재 페이지
-                itemsCountPerPage={limit} //한 페이지당 보여줄 리스트 아이템의 개수
-                totalItemsCount={total} //총 아이템의 개수
-                pageRangeDisplayed={10} //Paginator 내에서 보여줄 페이지의 범위(10개)
-                prevPageText={"‹"} //"이전"을 나타낼 텍스트
-                nextPageText={"›"} //"다음"을 나타낼 텍스트
-                onChange={handlePageChange} //페이지가 바뀔 때 핸들링해줄 함수
-                onClick={getList}
-            />
+            {/*<Pagination*/}
+            {/*    activePage={page} //현재 페이지*/}
+            {/*    itemsCountPerPage={limit} //한 페이지당 보여줄 리스트 아이템의 개수*/}
+            {/*    totalItemsCount={total} //총 아이템의 개수*/}
+            {/*    pageRangeDisplayed={10} //Paginator 내에서 보여줄 페이지의 범위(10개)*/}
+            {/*    prevPageText={"‹"} //"이전"을 나타낼 텍스트*/}
+            {/*    nextPageText={"›"} //"다음"을 나타낼 텍스트*/}
+            {/*    onChange={handlePageChange} //페이지가 바뀔 때 핸들링해줄 함수*/}
+            {/*    onClick={getList}*/}
+            {/*/>*/}
         </div>
     )
 }
