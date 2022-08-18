9aa632e5f7f25aca2d6ad4e7c77c6645fe708b71
jkc-mycode <jkc9709@naver.com>
2022-08-19 01:23:08 +0900

    2022.08.19 댓글, 대댓글 수정 axios 틀 구현

--- src/Components/recomments.js
+++ src/Components/recomments.js
@@ -9,7 +9,10 @@ const Recomments = (props) => {
     console.log(props);
     const [commentsList, setCommentsList] = useState([]); //보여줄 comments 리스트
     const [content, setContent] = useState(""); //댓글 내용
+    // const [reCommentContent, setReCommentContent] = useState(""); //대댓글 내용
     const [isReComment, setIsReComment] = useState(false);
+    const [isUpdateButton, setIsUpdateButton] = useState(false);
+    // const [isReCommentUpdateButton, setIsReCommentUpdateButton] = useState(false);
 
     function timer(d) {
         let timestamp = d;
@@ -27,8 +30,21 @@ const Recomments = (props) => {
     }
 
     const onCheckRecomment = () => {
+        setContent("");
         setIsReComment(!isReComment);
+        setIsUpdateButton(false);
     }
+
+    const onCheckUpdate = (e) => {
+        setContent(e.currentTarget.value);
+        setIsUpdateButton(!isUpdateButton);
+        setIsReComment(false);
+    }
+
+    // const onCheckReCommentUpdate = (e) => {
+    //     setReCommentContent(e.currentTarget.value);
+    //     setIsReCommentUpdateButton(!isReCommentUpdateButton);
+    // }
 
     //로그인 유무 확인
     const isLogin = () => {
@@ -48,6 +64,11 @@ const Recomments = (props) => {
 
     //댓글 작성 axios
     const commentOnSubmit = async () => {
+        // if(!`${content}`){
+        //     data = {content: `${reCommentContent}`}
+        // }else{
+        //     data = {content: `${content}`}
+        // }
         const res = await axios.post('/comment/' + props.comment._id, data, headers)
             .then((res) => {
                 console.log(res);
@@ -78,6 +99,7 @@ const Recomments = (props) => {
             const res = await axios.delete('/comment/' + e.currentTarget.value)
                 .then((res) => {
                     alert("댓글이 삭제되었습니다.");
+                    window.location.reload();
                     console.log(res);
                 })
                 .catch((e) => {
@@ -86,6 +108,11 @@ const Recomments = (props) => {
         }else{
             console.log("취소");
         }
+    }
+
+    //댓글 수정 axios
+    const updateComment = async () => {
+
     }
 
     useEffect(() => {
@@ -99,32 +126,56 @@ const Recomments = (props) => {
                     props.comment.isDeleted === true
                         ? null
                         : <>
-                            <div className="comment_update">수정</div>
+                            <button type="button" className="comment_update" value={props.comment.content} onClick={onCheckUpdate}>수정</button>
                             <button type="button" className="comment_delete" value={props.comment._id} onClick={deleteComment}>삭제</button>
-                            <div className="recomment_button" onClick={onCheckRecomment}>댓글달기</div>
+                            {
+                                props.comment.isRecomment === false
+                                    ? <button type="button" className="recomment_button" onClick={onCheckRecomment}>댓글달기</button>
+                                    : null
+                            }
                         </>
                 }
                 <hr/>
-            {
-                isReComment === true
-                    ? <div className="comments_header">
-                            <TextField
-                                className="comments_header_textarea"
-                                maxRows={3}
-                                onClick={isLogin}
-                                onChange={(e) => {
-                                    setContent(e.target.value)
-                                }}
-                                multiline placeholder="댓글을 입력해주세요✏️"
-                            />
-                            {
-                                content !== ""
-                                    ? <Button variant="outlined" onClick={commentOnSubmit}>등록하기</Button>
-                                    : <Button variant="outlined" disabled={true}>등록하기</Button>
-                            }
-                        </div>
-                    : null
-            }
+                {
+                    isReComment === true
+                        ? <div className="comments_header">
+                                <TextField
+                                    className="comments_header_textarea"
+                                    maxRows={3}
+                                    onClick={isLogin}
+                                    onChange={(e) => {
+                                        setContent(e.target.value)
+                                    }}
+                                    value={content}
+                                    multiline placeholder="댓글을 입력해주세요✏️"
+                                />
+                                {
+                                    content !== ""
+                                        ? <Button variant="outlined" onClick={commentOnSubmit}>등록하기</Button>
+                                        : <Button variant="outlined" disabled={true}>등록하기</Button>
+                                }
+                            </div>
+                        : ( isUpdateButton === true
+                                ? <div className="comments_header">
+                                    <TextField
+                                        className="comments_header_textarea"
+                                        maxRows={3}
+                                        onClick={isLogin}
+                                        onChange={(e) => {
+                                            setContent(e.target.value)
+                                        }}
+                                        value={content}
+                                        multiline placeholder="댓글을 입력해주세요✏️"
+                                    />
+                                    {
+                                        content !== content
+                                            ? <Button variant="outlined" onClick={updateComment}>수정하기</Button>
+                                            : <Button variant="outlined" disabled={true}>수정하기</Button>
+                                    }
+                                </div>
+                                : null
+                        )
+                }
             </div>
             <div className="recomments_body">
                 {
@@ -136,14 +187,48 @@ const Recomments = (props) => {
                                 </div>
                                 <div className="comment_content">{item.content}</div>
                                 <div className="comment_username">{item.author}</div>
-                                <div className="comment_update">수정</div>
-                                <button type="button" className="comment_delete" value={item._id} onClick={deleteComment}>삭제</button>
+                                {/*<button type="button" className="comment_update" value={item.content} onClick={onCheckReCommentUpdate}>수정</button>*/}
+                                {/*<button type="button" className="comment_delete" value={item._id} onClick={deleteComment}>삭제</button>*/}
                             </div>
-                            {
-                                commentsList.indexOf(item) !== commentsList.length-1
-                                ? <hr/>
-                                : null
-                            }
+                            <div className="recomment_box">
+                                <Recomments post_id={item._id} comment={item}></Recomments>
+                            </div>
+                            {/*{*/}
+                            {/*    commentsList.indexOf(item) !== commentsList.length-1*/}
+                            {/*    ? <hr/>*/}
+                            {/*    : null*/}
+                            {/*}*/}
+                            {/*{*/}
+                            {/*    isReCommentUpdateButton === true*/}
+                            {/*        ? <div className="comments_header">*/}
+                            {/*            {*/}
+                            {/*                commentsList.indexOf(item) !== 0*/}
+                            {/*                    ? <hr/>*/}
+                            {/*                    : null*/}
+                            {/*            }*/}
+                            {/*            <TextField*/}
+                            {/*                className="comments_header_textarea"*/}
+                            {/*                maxRows={3}*/}
+                            {/*                onClick={isLogin}*/}
+                            {/*                onChange={(e) => {*/}
+                            {/*                    setReCommentContent(e.target.value)*/}
+                            {/*                }}*/}
+                            {/*                value={reCommentContent}*/}
+                            {/*                multiline placeholder="댓글을 입력해주세요✏️"*/}
+                            {/*            />*/}
+                            {/*            {*/}
+                            {/*                reCommentContent !== reCommentContent*/}
+                            {/*                    ? <Button variant="outlined" onClick={updateComment}>수정하기</Button>*/}
+                            {/*                    : <Button variant="outlined" disabled={true}>수정하기</Button>*/}
+                            {/*            }*/}
+                            {/*            {*/}
+                            {/*                commentsList.indexOf(item) !== commentsList.length-1*/}
+                            {/*                    ? <hr/>*/}
+                            {/*                    : null*/}
+                            {/*            }*/}
+                            {/*        </div>*/}
+                            {/*        : null*/}
+                            {/*}*/}
                         </>
                     ))
                 }
