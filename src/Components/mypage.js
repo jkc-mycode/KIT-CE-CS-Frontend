e5833df0ddfedd0f3581de66c174c8f9e6dc292c
jkc-mycode <jkc9709@naver.com>
2022-08-23 19:58:13 +0900

    2022.08.23 마이페이지 유저등급변경, MyPost 보여주기 및 페이지네이션 구현

--- src/Components/mypage.js
+++ src/Components/mypage.js
@@ -1,22 +1,40 @@
 import React, {useState, useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, Link} from 'react-router-dom';
 import axios from 'axios';
 import './mypage.css';
 import { getCookie, removeCookie } from '../cookie';
+import Pagination from "react-js-pagination";
 
 function MyInfoPage(){
+    //신고자리스트 페이지네이션
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(2); //20개 고정
+    const [total, setTotal] = useState(12); //전체 게시물 수
+    let reportNum = 0; //신고리스트 num
+
+    //유저정보 및 페이지네이션
     const [user, setUser] = useState([]); //user 정보
     const [reportList, setReportList] = useState([]);
     const [myArticle, setMyArticle] = useState([]); //게시물
+    const [postPage, setPostPage] = useState(1);
+    const [postLimit, setPostLimit] = useState(2); //20개 고정
+    const [postTotal, setPostTotal] = useState(12); //전체 게시물 수
+    const [num, setNum] = useState(0); //각 페이지 제일 윗번호
+    let x = -1;
+
+    //비밀번호
     const [currentPassword, setCurrentPassword] = useState(""); //기존 비밀번호
     const [newPassword, setNewPassword] = useState(""); //새로운 비밀번호
     const [deleteAccountPassword, setDeleteAccountPassword] = useState(""); //탈퇴 비밀번호 확인
-
     const [confirmPassword, setConfirmPassword] = useState(""); //새로운 비밀번호 확인
     const [pwCheckMsg, setpwCheckMsg] = useState(""); // 비밀번호 확인 메시지
     const [pwMsgBool, setpwMsgBool] = useState(false); //같은지 유무 메시지
     const navigate = useNavigate();
     //let num = myArticle.length; //article 길이
+
+    //유저등급변경
+    const [userId, setUserId] = useState("");
+    const [grade, setGrade] = useState(0);
 
     function timer(d){
         let timestamp = d;
@@ -32,17 +50,16 @@ function MyInfoPage(){
         let returnDate = year + "/" + month + "/" + day + "/ " + hour + ":" + minute + ":" + second;
         return returnDate;
     }
-    
-    useEffect(() => {
-        checkLogin();
-    }, [])
 
-    const checkLogin = () => {
-        if (!getCookie('kit_acs')) {
-            alert("로그인이 필요합니다.")
-            navigate("/login");
-        }
-    }
+    //게시물리스트 페이지네이션
+    const handlePostPageChange = (page) => {
+        setPostPage(page);
+    };
+
+    //신고리스트 페이지네이션
+    const handlePageChange = (page) => {
+        setPage(page);
+    };
 
     //비밀번호 변경관련 핸들러
     const onCurrentPasswordHandler = (event) => {
@@ -71,6 +88,30 @@ function MyInfoPage(){
             setpwMsgBool(true);
             setpwCheckMsg("비밀번호가 일치합니다.");
         }
+    }
+
+    //유저등급 변경관련 핸들러
+    const onUserIdHandler = (e) => {
+        setUserId(e.currentTarget.value);
+    }
+    const onGradeHandler = (e) => {
+        setGrade(e.currentTarget.value);
+    }
+
+    //mypage 정보 가져오는 axios
+    const getUserInfo = async () =>{
+        const res = await axios.get('/mypage/?page=' + postPage)
+            .then((res) => {
+                console.log(res.data);
+                setUser(res.data.userMypage);
+                setMyArticle(res.data.userMypage.articles)
+                setPostTotal(res.data.totalArticle);
+                setPostLimit(res.data.postLimit);
+                setNum(res.data.totalArticle - (postPage * res.data.postLimit)+res.data.postLimit);
+            })
+            .catch((e) => {
+                console.log(e);
+            })
     }
 
     //비밀번호 변경 axios
@@ -123,10 +164,12 @@ function MyInfoPage(){
 
     //신고 리스트 가져오는 axios
     const getReportList = async () => {
-        const res = await axios.get('/report')
+        const res = await axios.get('/report?page=' + page)
             .then((res) => {
-                console.log(res.data.reports);
+                console.log(res.data);
                 setReportList(res.data.reports);
+                setTotal(res.data.totalReport);
+                setLimit(res.data.postLimit);
             })
             .catch((e) => {
                 console.log(e);
@@ -149,25 +192,41 @@ function MyInfoPage(){
         }
     }
 
+
+    //유저등급 변경 axios
+    let data = {
+        id: `${userId}`,
+        class: `${grade}`
+    }
+    const headers = {
+        "Content-Type": `application/json`,
+    };
+    const gradeChange = () => {
+        if(window.confirm("변경하시겠습니까?")){
+            axios.patch('/mypage/class', data, headers)
+                .then((res) => {
+                    console.log(res);
+                    alert("변경되었습니다!");
+                    window.location.reload();
+                })
+                .catch((e) => {
+                    console.log(e);
+                })
+        }else{
+            console.log("취소");
+        }
+    }
+
     useEffect(() => {
         if(!getCookie('kit_acs')){
             alert("로그인 후 이용가능!!");
             navigate("/");
         }
-        //mypage 정보 가져오는 axios
-        axios.get('/mypage/')
-            .then((res) => {
-                console.log(res.data);
-                setUser(res.data);
-                setMyArticle(res.data.article)
-            })
-            .catch((e) => {
-                console.log(e);
-            })
-        if(user.id === "admin"){
-            getReportList();
+        if(getCookie('kit_acs_class') === "3"){
+           getReportList();
         }
-    }, [])
+        getUserInfo();
+    }, [page, postPage])
 
     return (
         <div className='view_section'>
@@ -203,28 +262,22 @@ function MyInfoPage(){
                         </div>
                     </form>
                 </div>
-                <div className='admin_box'>
-                    <h1>&#xE001;_ UserGrade</h1>
-                </div>
+                {
+                    user.id === "admin"
+                        ? <div className='grade_box'>
+                            <h1>&#xE001;_ UserGrade</h1>
+                            <form>
+                                <input type="text" name="user" placeholder="아이디 입력" onChange={onUserIdHandler}/>
+                                <input type="text" name="grade" placeholder="등급 입력" onChange={onGradeHandler}/>
+                                <button type="button" onClick={gradeChange}>변경하기</button>
+                            </form>
+                        </div>
+                        : null
+                }
             </div>
             <div className="margin_section"></div>
             <div className='right_mypage'>
                 <div className="mypost_box">
-                    {/* {
-                        myArticle.slice(0).reverse().map((i) => {
-                            let goView = (e) => {
-                                navigate('view/'+i._id, {state : i});
-                            }
-                            return (
-                                <tr onClick={goView}>
-                                    <td>1{num--}</td>
-                                    <td>title{i.title}</td>
-                                    <td>date{timer(i.date)}</td>
-                                    <td>2{i.commentList.length}</td>
-                                </tr>
-                            )
-                        })
-                    } */}
                     <h1>&#xE001;_ MyPost</h1>
                     <table>
                         <tr>
@@ -233,13 +286,35 @@ function MyInfoPage(){
                             <th>작성일</th>
                             <th>댓글수</th>
                         </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>title</td>
-                            <td>date</td>
-                            <td>2z</td>
-                        </tr>
+                        {
+                            myArticle.slice(0).map((i) => {
+                                x = x + 1;
+                                let goView = (e) => {
+                                    navigate('/view/'+i._id, {state : i});
+                                }
+                                return (
+                                    <tr onClick={goView}>
+                                        <td>{num-x}</td>
+                                        <td>
+                                            <div className="title_length">{i.title}</div>
+                                        </td>
+                                        <td>{timer(i.date)}</td>
+                                        <td>{i.commentList.length}</td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </table>
+                    <Pagination
+                        activePage={postPage} //현재 페이지
+                        itemsCountPerPage={postLimit} //한 페이지당 보여줄 리스트 아이템의 개수
+                        totalItemsCount={postTotal} //총 아이템의 개수
+                        pageRangeDisplayed={4} //Paginator 내에서 보여줄 페이지의 범위(10개)
+                        prevPageText={"‹"} //"이전"을 나타낼 텍스트
+                        nextPageText={"›"} //"다음"을 나타낼 텍스트
+                        onChange={handlePostPageChange} //페이지가 바뀔 때 핸들링해줄 함수
+                        onClick={getUserInfo}
+                    />
                 </div>
                 {
                     user.id === "admin"
@@ -249,7 +324,7 @@ function MyInfoPage(){
                                 <tr>
                                     <th>번호</th>
                                     <th>카테고리</th>
-                                    <th>ID</th>
+                                    <th>링크</th>
                                     <th>신고사유</th>
                                     <th>신고자</th>
                                     <th>신고일</th>
@@ -258,6 +333,7 @@ function MyInfoPage(){
                                 {
                                     reportList.map((item) => {
                                         let type = null;
+                                        reportNum++;
                                         if(item.targetType === "article"){
                                             type = "게시물"
                                         }else{
@@ -265,9 +341,9 @@ function MyInfoPage(){
                                         }
                                         return(
                                             <tr>
-                                                <td>1</td>
+                                                <td>{reportNum}</td>
                                                 <td>{type}</td>
-                                                <td>여긴?</td>
+                                                <td><Link to={`/view/${item.articleId}`}>바로가기</Link></td>
                                                 <td>{item.reason}</td>
                                                 <td>{item.reporter}</td>
                                                 <td>{timer(item.date)}</td>
@@ -279,6 +355,16 @@ function MyInfoPage(){
                                     })
                                 }
                             </table>
+                            <Pagination
+                                activePage={page} //현재 페이지
+                                itemsCountPerPage={limit} //한 페이지당 보여줄 리스트 아이템의 개수
+                                totalItemsCount={total} //총 아이템의 개수
+                                pageRangeDisplayed={4} //Paginator 내에서 보여줄 페이지의 범위(10개)
+                                prevPageText={"‹"} //"이전"을 나타낼 텍스트
+                                nextPageText={"›"} //"다음"을 나타낼 텍스트
+                                onChange={handlePageChange} //페이지가 바뀔 때 핸들링해줄 함수
+                                onClick={getReportList}
+                            />
                         </div>
                         : null
                 }
