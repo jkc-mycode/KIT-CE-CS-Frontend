e95031d5a813dee992c5ed197aabb3f2616055e8
jkc-mycode <jkc9709@naver.com>
2022-08-30 02:09:46 +0900

    2022.08.30 아이디찾기/비밀번호재설정 수정중

--- src/Components/idsearch.js
+++ src/Components/idsearch.js
@@ -5,9 +5,13 @@ import './idsearch.css';
 function IDSearch (){
     const [name, setName] = useState("");
     const [webmail, setWebmail] = useState("");
+    const [pwName, setPwName] = useState("");
+    const [id, setId] = useState("");
 
     const [nameCheck, setNameCheck] = useState(false);
     const [webmailCheck, setWebmailCheck] = useState(false);
+    const [pwNameCheck, setPwNameCheck] = useState(false);
+    const [idCheck, setIdCheck] = useState(false);
 
     const onNameHandler = useCallback((e) => {
         const nameRegExp = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,5}$/;
@@ -27,15 +31,34 @@ function IDSearch (){
             setWebmailCheck(true);
         }
     }, [])
-    
-    let data = {
-        name: `${name}`,
-        webmail: `${webmail}@kumoh.ac.kr`
-    }
+    const onPwNameHandler = useCallback((e) => {
+        const nameRegExp = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,5}$/;
+        setPwName(e.currentTarget.value);
+        if (!nameRegExp.test(e.currentTarget.value)) {
+            setPwNameCheck(false);
+        } else {
+            setPwNameCheck(true);
+        }
+    }, [])
+    const onIdHandler = useCallback((e) => {
+        const idRegExp = /^[a-z0-9]{5,10}$/;
+        setId(e.currentTarget.value);
+        if (!idRegExp.test(e.currentTarget.value)) {
+            setIdCheck(false);
+        } else {
+            setIdCheck(true);
+        }
+    }, [])
+
+    //아이디찾기 axios
     const headers = {
         "Content-Type": `application/json`,
     };
-    const submitUserInfo = async () => {
+    const submitSearchId = async () => {
+        let data = {
+            name: `${name}`,
+            webmail: `${webmail}@kumoh.ac.kr`
+        }
         console.log(name, nameCheck);
         console.log(webmail, webmailCheck);
         if(!nameCheck){
@@ -46,6 +69,31 @@ function IDSearch (){
             const res = await axios.post('/sign/findID', data, headers)
                 .then((res) => {
                     alert(name + "님의 아이디 : " + res.data);
+                    window.location.reload();
+                })
+                .catch((e) => {
+                    console.log(e);
+                    alert("일치하는 정보가 없습니다.");
+                })
+        }
+    }
+
+    //비밀번호 재설정 axios
+    const submitSearchPw = async () => {
+        let data = {
+            name: `${pwName}`,
+            id: `${id}`
+        }
+        if(!pwNameCheck){
+            alert("이름이 잘못 입력되었습니다.");
+        }else if(!idCheck){
+            alert("아이디가 잘못 입력되었습니다.")
+        }else{
+            const res = await axios.post('/sign/findPassword', data, headers)
+                .then((res) => {
+                    console.log(res);
+                    alert("웹메일로 임시 비밀번호 링크를 전송했습니다.");
+                    window.location.replace('/login');
                 })
                 .catch((e) => {
                     console.log(e);
@@ -63,7 +111,15 @@ function IDSearch (){
                     <input type="text" className="id_search_name" onChange={onNameHandler}/>
                     <div>웹메일</div>
                     <input type="text" className="id_search_webmail" onChange={onWebmailHandler}/>@kumoh.ac.kr<br/><br/>
-                    <button type="button" className="id_search_button" onClick={submitUserInfo}>아이디찾기</button>
+                    <button type="button" className="id_search_button" onClick={submitSearchId}>아이디찾기</button>
+                </div>
+                <div className="pw_search_box">
+                    <h1>&#xE001;_ 비밀번호 재설정</h1>
+                    <div>이름</div>
+                    <input type="text" className="pw_search_name" onChange={onPwNameHandler}/>
+                    <div>아이디</div>
+                    <input type="text" className="pw_search_id" onChange={onIdHandler}/><br/><br/>
+                    <button type="button" className="pw_search_button" onClick={submitSearchPw}>비밀번호재설정</button>
                 </div>
             </div>
         </>
