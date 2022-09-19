9630c527d804303f90e57825035fd3e5bfc65eb3
jeongchan12 <jkc9709@hanmail.net>
2022-09-19 20:23:09 +0900

    2022.09.19 쿠키 수정

--- src/cookie.js
+++ src/cookie.js
@@ -11,5 +11,5 @@ export const getCookie = (name) => {
 }
 
 export const removeCookie = (name) => {
-    return cookies.remove(name, { domain: "localhost", path: "/" })
+    return cookies.remove(name, { domain: "kitacs.com", path: "/" })
 }
